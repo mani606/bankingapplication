c95bced75dea81cf1fc126b5db8cc68abda4ee14
mani606 <57083158+mani606@users.noreply.github.com>
2020-06-11 21:41:14 +0530

    Some changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AcoountSummaryComponent } from './acoount-summary/acoount-summary.component';
 import { FundTransferComponent } from './fund-transfer/fund-transfer.component';
 import { TransactionsComponent } from './transactions/transactions.component';
-
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path: "", component: LoginComponent},
@@ -13,7 +12,7 @@ const routes: Routes = [
   {path: 'summary',component: AcoountSummaryComponent},
   {path: 'fundtranfer',component: FundTransferComponent},
   {path: 'transactions',component: TransactionsComponent}
-];
+ ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
